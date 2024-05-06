c8bb378b165871bebcb4e5c160fe2682cac3e183
Kamau <kamauwamuigaijr@gmail.com>
2024-05-06 18:26:04 +0300

    complete lab

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const transactions = [
+    {
+      date: '2024-02-02',
+      description: 'Salary',
+      category: 'Income',
+      amount: 1000000
+    },
+    {
+      date: '2024-02-03',
+      description: 'Rent',
+      category: 'Bills',
+      amount: -5000
+    },
+    {
+      date: '2024-02-04',
+      description: 'Electricity Bill',
+      category: 'Bills',
+      amount: -3000
+    },
+    {
+      date: '2024-02-05',
+      description: 'Sports subscriptions',
+      category: 'Entertainment',
+      amount: -10000
+    },
+    {
+      date: '2024-04-04',
+      description: 'Online Shopping',
+      category: 'Fashion',
+      amount: -8000
+    },
+    {
+      date: '2024-04-05',
+      description: 'Pizza Delivery',
+      category: 'Food',
+      amount: -500
+    },
+    {
+      date: '2024-04-06',
+      description: 'Movies',
+      category: 'Entertainment',
+      amount: -100
+    }
+  ];
+  
